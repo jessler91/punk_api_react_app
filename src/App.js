f5b44fa4617315e39a61123a6994f21ab8037843
jessler91 <justinessler@fastmail.com>
2020-05-11 09:27:05 -0500

    not as good as I want, but need to turn it in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
 
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+        isLoaded: false,
+        beerList: {}
+    };
+  }
+    
+
+  componentDidMount() {
+    fetch("https://api.punkapi.com/v2/beers")
+        .then(res => res.json())
+        .then((beerList) => { 
+          this.setState({
+            isLoaded: true,
+            beerList: beerList
+            // I am not a huge fan of the.. if names match, then drop the double reference
+          });
+        })
+        .then(console.log(this.state.beerList))
+        .catch(error => console.log('FETCH ERROR', error))
+    }
+  
+
+  render() {
+    const { isLoaded } = this.state;
+    
+    if (!isLoaded) { 
+      return <div>Loading, Please Wait...</div>;
+    } 
+    else { return (
+        <div class='container'>
+          <header className="App-header">Justin Essler - Beer API </header>
+          <ul>{this.state.beerList.map((beerList, index) => 
+            <li key={index}>
+              <img alt="beer_image" src={beerList.image_url}></img>
+              <p>{beerList.name}</p>
+              <p>{beerList.tagline}</p> 
+            </li>)}
+          </ul>
+        </div>
+      );
+    }
+  }
+
+
 }
 
 export default App;
